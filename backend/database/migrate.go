93b525dc9cd5a254de667a6c66833ece831816aa
andyosyndoh <andyovvo8@gmail.com>
2025-09-06 11:11:08 +0300

    refactor: remove unused import in migrate.go

--- backend/database/migrate.go
+++ backend/database/migrate.go
@@ -3,20 +3,12 @@ package database
 import (
 	"log"
 	"task/backend/models"
-
-	"gorm.io/gorm"
 )
 
 func RunMigrations() {
 	log.Println("Running database migrations...")
 
-	// Drop the tasks table if it exists
-	err := DB.Migrator().DropTable(&models.Task{})
-	if err != nil && err != gorm.ErrCantStartTransaction {
-		log.Printf("Warning: Could not drop table 'tasks'. It might not exist or there's another issue: %v", err)
-	}
-
-	err = DB.AutoMigrate(
+	err := DB.AutoMigrate(
 		&models.Task{},
 	)
 
