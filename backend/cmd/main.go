d9e6c83649e1937c50543d6704ae5161319ee1c8
andyosyndoh <andyovvo8@gmail.com>
2025-09-06 08:09:59 +0300

    feat: add router

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -13,7 +13,6 @@ func main() {
 
 	// Add CORS middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
